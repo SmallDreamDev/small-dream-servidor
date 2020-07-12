19bc22ab581b940970ef5f1b2d5d1be297734a09
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-12 18:38:06 +0000

    Deploy AdrianPerezManso/small-dream to github.com/AdrianPerezManso/small-dream.git:gh-pages

---
+++ precache-manifest.92e3de299d71c19e81fb300c0deb134e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7eb279289ec90c1598e7525de12b214",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d802bfc3741e3550707e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "780721c691c9fefca516",
+    "url": "/static/js/2.2becf59f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2becf59f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d802bfc3741e3550707e",
+    "url": "/static/js/main.fb537a43.chunk.js"
+  },
+  {
+    "revision": "fe9c57a54dc95d1d7be1",
+    "url": "/static/js/runtime-main.2edb97f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
