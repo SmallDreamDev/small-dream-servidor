ece1dd84218b610941bc4098060646c988ff5419
Adrián <adrianoelmagno@gmail.com>
2020-07-30 19:25:11 +0200

    Acabada fábrica y CRUD. Arreglado algún error menor

--- src/modules/crud.js
+++ src/modules/crud.js
@@ -1,7 +1,7 @@
 module.exports = {
     gestorBD: null,
     factory: null,
-    init: function (gestorBD) {
+    init: function (gestorBD, factory) {
         this.gestorBD = gestorBD;
         this.factory = factory;
     },
@@ -25,5 +25,93 @@ module.exports = {
                 });
             }
         })
+    },
+    createActivity: function(requestBody, functionCallback){
+        this.factory.createActivity(requestBody, function(activity){
+            if(activity === null){
+                functionCallback(null);
+            }else{
+                this.gestorBD.insertEntity("actividades", activity, function(id){
+                    functionCallback(id);
+                });
+            }
+        });
+    },
+    createCategory: function(requestBody, functionCallback){
+        this.factory.createCategory(requestBody, function(category){
+            if(category === null){
+                functionCallback(null);
+            }else{
+                this.gestorBD.insertEntity("categorias", category, function(id){
+                    functionCallback(id);
+                });
+            }
+        });
+    },
+    createInstructor: function(requestBody, functionCallback){
+        this.factory.createActivity(requestBody, function(instructor){
+            if(instructor === null){
+                functionCallback(null);
+            }else{
+                this.gestorBD.insertEntity("monitores", instructor, function(id){
+                    functionCallback(id);
+                });
+            }
+        });
+    },
+    createMaterial: function(requestBody, functionCallback){
+        this.factory.createActivity(requestBody, function(material){
+            if(material === null){
+                functionCallback(null);
+            }else{
+                this.gestorBD.insertEntity("materiales", material, function(id){
+                    functionCallback(id);
+                });
+            }
+        });
+    },
+    createCategory: function(requestBody, functionCallback){
+        this.factory.createActivity(requestBody, function(category){
+            if(category === null){
+                functionCallback(null);
+            }else{
+                this.gestorBD.insertEntity("categorias", category, function(id){
+                    functionCallback(id);
+                });
+            }
+        });
+    },
+    createProgram: function(requestBody, functionCallback){
+        this.factory.createActivity(requestBody, function(program){
+            if(program === null){
+                functionCallback(null);
+            }else{
+                this.gestorBD.insertEntity("programas", program, function(id){
+                    functionCallback(id);
+                });
+            }
+        });
+    },
+    createSchedule: function(requestBody, functionCallback){
+        this.factory.createActivity(requestBody, function(schedule){
+            if(schedule === null){
+                functionCallback(null);
+            }else{
+                this.gestorBD.insertEntity("horarios", schedule, function(id){
+                    functionCallback(id);
+                });
+            }
+        });
+    },
+    createWorkshop: function(requestBody, functionCallback){
+        this.factory.createActivity(requestBody, function(workshop){
+            if(workshop === null){
+                functionCallback(null);
+            }else{
+                this.gestorBD.insertEntity("talleres", workshop, function(id){
+                    functionCallback(id);
+                });
+            }
+        });
     }
 };
--- src/modules/factory.js
+++ src/modules/factory.js
@@ -13,5 +13,66 @@ module.exports = {
         this.validations.checkValidClient(clientToValidate, function(client){
             functionCallback(client);
         });
+    },
+    createActivity(requestBody, functionCallback){
+        let activityToValidate = {
+            nombre: requestBody.nombre,
+            zona: requestBody.zona
+        }
+        this.validations.checkValidActivity(activityToValidate, function(activity){
+            functionCallback(activity);
+        });
+    },
+    createCategory(requestBody, functionCallback){
+        let categoryToValidate = {
+            nombre: requestBody.nombre,
+            zona: requestBody.zona
+        }
+        this.validations.checkValidCategory(categoryToValidate, function(category){
+            functionCallback(category);
+        });
+    },
+    createInstructor(requestBody, functionCallback){
+        let instructorToValidate = {
+            nombre_completo: requestBody.nombre_completo,
+            dni: requestBody.dni,
+            contacto: requestBody.contacto
+        }
+        this.validations.checkValidInstructor(instructorToValidate, function(instructor){
+            functionCallback(instructor);
+        });
+    },
+    createMaterial(requestBody, functionCallback){
+        let materialToValidate = {
+            precio: requestBody.precio,
+            descripcion: requestBody.descripcion
+        }
+        this.validations.checkValidMaterial(materialToValidate, function(material){
+            functionCallback(material);
+        });
+    },
+    createSchedule(requestBody, functionCallback){
+        let scheduleToValidate = {
+            hora_inicio: requestBody.hora_inicio,
+            hora_fin: requestBody.hora_fin,
+            id_actividad: requestBody.id_actividad,
+            id_monitor: requestBody.id_monitor,
+            fecha: requestBody.fecha
+        }
+        this.validations.checkValidSchedule(scheduleToValidate, function(schedule){
+            functionCallback(schedule);
+        });
+    },
+    createWorkshop(requestBody, functionCallback){
+        let workshopToValidate = {
+            id_monitor: requestBody.id_monitor,
+            id_actividad: requestBody.id_actividad,
+            fecha: requestBody.fecha,
+            plazas: requestBody.plazas,
+            id_modo_pago: requestBody.id_modo_pago
+        }
+        this.validations.checkValidWorkshop(workshopToValidate, function(workshop){
+            functionCallback(workshop);
+        });
     }
 }
